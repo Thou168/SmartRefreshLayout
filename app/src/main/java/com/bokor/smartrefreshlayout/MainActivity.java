3054d43c4e606a599679a0d76b5a090fa9ec2bf5
Thou <Senthou168@gmail.com>
2019-09-18 16:17:58 +0700

    Initial commit

---
+++ app/src/main/java/com/bokor/smartrefreshlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bokor.smartrefreshlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
